50f67e418e110df980a412c502074dfa26d12d1d
Abhishek Srivastava <srivastava.abhishek.sa@gmail.com>
2017-10-26 02:36:52 -0400

    Added setup.py as a utility. Added function createCentroids to create random centroids and write to centroids.txt file

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+import random
+
+def createCentroidsFile(k, inputFile):
+
+    with open(inputFile) as f:
+        data = f.readline().split()
+
+    random.shuffle(data)
+    centroids = data[:k]
+
+    with open('centroids1.txt', 'w') as f:
+        f.write('\t'.join(centroids))
+
+    print("Created centroids.txt with centroids as :{}".format(centroids))
+
+
+
+if __name__ == '__main__':
+    inputFile = input("Enter the name of the input file: ")
+    k = int(input("Enter the number of clusters desired: "))
+    createCentroidsFile(k, inputFile)
